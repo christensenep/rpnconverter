2037ef74a567726fd960e329764b1106f845d264
Erik Christensen <christensenep@gmail.com>
2016-11-23 14:47:08 -0500

    Test throwing error on operator at start of expression

--- test/rpnconverter.js
+++ test/rpnconverter.js
@@ -66,4 +66,9 @@ describe('Infix -> Postfix error handling', function() {
     let func = infixToPostfix.bind(this, infixString);
     expect(func).to.throw(Error, "Operand 'c' is in an invalid position");
   });
+  it('should error on operator at start of expression', function() {
+    let infixString = '-a+c+d';
+    let func = infixToPostfix.bind(this, infixString);
+    expect(func).to.throw(Error, "Operator '-' is in an invalid position");
+  });
 });
