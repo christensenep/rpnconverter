a1069fbefb5cfe5dfd0b399d4dbfeb6547e3ebdd
Erik Christensen <christensenep@gmail.com>
2016-11-28 16:28:56 -0500

    Move helper function to bottom of file

--- rpnconverter.js
+++ rpnconverter.js
@@ -1,30 +1,3 @@
-function isOperator(char) {
-  let operatorRegex = /[-+/*^]/;
-  return char.match(operatorRegex) !== null;
-};
-
-function isOperand(char) {
-  let operandRegex = /[a-z]/;
-  return char.match(operandRegex) !== null;
-};
-
-function getPriority(operator) {
-  let operatorPrecedence = '+-*/^';
-  return operatorPrecedence.indexOf(operator);
-}
-
-function shouldGroupLeft(currentOperator, pendingOperators) {
-  if (pendingOperators.length === 0) {
-    return false;
-  }
-
-  let previousOperator = pendingOperators[pendingOperators.length - 1];
-
-  if (getPriority(previousOperator) >= getPriority(currentOperator)) {
-    return true;
-  }
-};
-
 function infixToPostfix(infixString) {
   let pendingOperators = [];
   let postfixString = '';
@@ -126,6 +99,33 @@ function postfixToInfix(postfixString) {
   }
 
   return infixString
+};
+
+function isOperator(char) {
+  let operatorRegex = /[-+/*^]/;
+  return char.match(operatorRegex) !== null;
+};
+
+function isOperand(char) {
+  let operandRegex = /[a-z]/;
+  return char.match(operandRegex) !== null;
+};
+
+function getPriority(operator) {
+  let operatorPrecedence = '+-*/^';
+  return operatorPrecedence.indexOf(operator);
+}
+
+function shouldGroupLeft(currentOperator, pendingOperators) {
+  if (pendingOperators.length === 0) {
+    return false;
+  }
+
+  let previousOperator = pendingOperators[pendingOperators.length - 1];
+
+  if (getPriority(previousOperator) >= getPriority(currentOperator)) {
+    return true;
+  }
 };
 
 module.exports = {
